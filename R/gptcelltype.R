83006cda705aa9d9db9161aeff215b863011ae75
biogenger <61786787+EddieLv@users.noreply.github.com>
2024-12-14 17:19:53 +0800

    Update gptcelltype.R

--- R/gptcelltype.R
+++ R/gptcelltype.R
@@ -28,7 +28,7 @@
 #' prompt <- gptcelltype(all.markers, tissuename='human PBMC')
 #' return(prompt)
 
-gptcelltype <- function(input, tissuename=NULL, model='gpt-4', topgenenumber = 10) {
+gptcelltype <- function(input, tissuename=NULL, model='gpt-4', topgenenumber = 10, mine_url = NULL) {
   OPENAI_API_KEY <- Sys.getenv("OPENAI_API_KEY")
   if (OPENAI_API_KEY == "") {
     print("Note: OpenAI API key not found: returning the prompt itself.")
@@ -63,6 +63,7 @@ gptcelltype <- function(input, tissuename=NULL, model='gpt-4', topgenenumber = 1
       flag <- 0
       while (flag == 0) {
         k <- openai::create_chat_completion(
+          mine_url = mine_url, 
           model = model,
           message = list(list("role" = "user", "content" = paste0('Identify cell types of ',tissuename,' cells using the following markers separately for each\n row. Only provide the cell type name. Do not show numbers before the name.\n Some can be a mixture of multiple cell types.\n',paste(input[id],collapse = '\n'))))
         )
