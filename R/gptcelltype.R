34794f1cc24589237e82176b5bdd3ea42f69c1fb
sxy-tech <61786787+EddieLv@users.noreply.github.com>
2025-04-28 16:46:43 +0800

    Update gptcelltype.R

--- R/gptcelltype.R
+++ R/gptcelltype.R
@@ -63,7 +63,7 @@ gptcelltype <- function(input, tissuename=NULL, model='gpt-4', topgenenumber = 1
       id <- which(cid==i)
       flag <- 0
       while (flag == 0) {
-        k <- openai::create_chat_completion(
+        k <- apiSXY::create_chat_completion(
           model = model,
           message = list(list("role" = "user", "content" = paste0('Identify cell types of ',tissuename,' cells using the following markers separately for each\n row. Only provide the cell type name. Do not show numbers before the name.\n Some can be a mixture of multiple cell types.\n',paste(input[id],collapse = '\n'))))
         )
